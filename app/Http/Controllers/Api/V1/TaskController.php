eb0f5cac0f900a37067a5fbf6ed038772d07c018
Kirill Dolbnya <kirilldolbnya6@gmail.com>
2025-03-25 18:12:50 +0300

    add task controller

---
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use http\Env\Request;
+
+class TaskController extends Controller
+{
+    public function create(Request $request)
+    {
+
+    }
+}
